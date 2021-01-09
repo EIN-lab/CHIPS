0ff92deffc3d498ace60eddcc7f63de58839288e
Hagai Har-Gil <=>
2021-01-09 05:51:01 +0200

    Made parseHeader more general

--- +utils/+scim/parseHeader.m
+++ +utils/+scim/parseHeader.m
@@ -1,20 +1,22 @@
-function header=parseHeader(input)
+function header = parseHeader(input, prefix)
 % PARSEHEADER   - Read ScanImage Header String and return structure.
 %   PARSEHEADER will output the value of the header fields as a structure.
 %   Input is the header from ScanImage TIF File (char array).
+%   prefix is the 'name' given to each data point
 %
 % See also
 
 out={};
+prefixLen = numel(prefix);
 tempcell=strread(input,'%q'); 
 for lineCounter=1:length(tempcell)
     data=tempcell{lineCounter};
-    if ~strncmp(data,'state.',6)
+    if ~strncmp(data, prefix, prefixLen)
         out{end}=[out{end} ' ' data(1:end-1)];
         continue
     end
     equal=findstr('=',data);
-    param=data(7:equal-1);
+    param=data(prefixLen + 1:equal-1);
     val=data(equal+1:end);
     if isempty(val)
         val=[];
